b8918912b37d79c71704bb926c394f5a5cb04747
k3vinrich4rd <kevinrichardve@gmail.com>
2022-11-08 09:31:19 -0300

    Inserindo implementações

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String nome = "Kevin";
+        int idade = 18;
+        double renda = 2000;
+        System.out.printf("%s tem %d anos de idade, e ganha R$ %.2f por mês ", nome, idade, renda);
+
     }
 }
